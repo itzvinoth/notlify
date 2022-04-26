aaf81226b5d414142fa3240974783d209753c628
mevinoth <vinoth.vinothrenga@gmail.com>
2022-04-26 18:03:41 +0530

    frontend code push

---
+++ notlify-front/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import { RouterLink, RouterView } from "vue-router";
+import WorkSpace from "@/components/WorkSpace.vue";
+</script>
+
+<template>
+	<div class="main-container">
+		<!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+		<div>Hello</div>
+		<div class="wrapper">
+			<WorkSpace />
+		</div>
+	</div>
+
+	<!-- <RouterView /> -->
+</template>
+
+<style lang="scss">
+@import "./assets/style.scss";
+</style>
