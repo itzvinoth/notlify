58208ae05108e5b01e1d7ff9b1400431a0307b0d
mevinoth <vinoth.vinothrenga@gmail.com>
2022-05-21 16:21:54 +0530

    feat: cards draggable around list

--- notlify-front/src/components/WorkSpace.vue
+++ notlify-front/src/components/WorkSpace.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="workspace">
-		<Transition></Transition>
-		<div class="drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
+		<!-- <div class="drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
 			<div v-for="item in getList(1)" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">
 				{{ item.title }}
 			</div>
@@ -17,6 +16,12 @@
 			<div v-for="item in getList(2)" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">
 				{{ item.title }}
 			</div>
+		</div> -->
+		<div class="drop-zone" v-for="list in computedList" :key="list.key" @drop="onDrop($event, list.key)" @dragenter.prevent @dragover.prevent>
+			<div class="list-title">{{ list.title }}</div>
+			<div v-for="card in list.cards" :key="`${list.key}+${card.id}`" class="drag-el" draggable="true" @dragstart="startDrag($event, card, list.key)">
+				{{ card.title }}
+			</div>
 		</div>
 	</div>
 </template>
@@ -26,13 +31,48 @@ export default {
 	name: 'WorkSpace',
 	data () {
 		return {
+			draggableCardOriginListId: null,
 			cardTitle: '',
 			isAddingCard: false,
 			listItems: [
 				{ id: 0, title: 'John', list: 1 },
 				{ id: 1, title: 'Joao', list: 1 },
 				{ id: 2, title: 'Jean', list: 2 }
+			],
+			lists: [
+				{
+					"key": 0,
+					"title": "User manual",
+					"cards": [
+						{ "id": 0, "title": "Set of lists is a board" },
+						{ "id": 1, "title": "Set of cards is a list" },
+						{ "id": 2, "title": "All data is saved" }
+					]
+				},
+				{
+					"key": 1,
+					"title": "Things to try",
+					"cards": [
+						{ "id": 0, "title": "Click on a card to edit" },
+						{ "id": 1, "title": "Click save after editing" },
+						{ "id": 2, "title": "To discard chages press esc" }
+					]
+				},
+				{
+					"key": 2,
+					"title": "More to try",
+					"cards": [
+						{ "id": 0, "title": "Drag notes around to rearrange." },
+						{ "id": 1, "title": "Works between the lists too." },
+						{ "id": 2, "title": "Try adding a new list." }
+					]
+				}
 			]
+		}
+	},
+	computed: {
+		computedList () {
+			return this.lists
 		}
 	},
 	methods: {
@@ -56,15 +96,20 @@ export default {
 		getList (list) {
 			return this.listItems.filter((item) => item.list === list)
 		},
-		startDrag (event, item) {
+		startDrag (event, card, listKey) {
+			this.draggableCardOriginListId = listKey
 			event.dataTransfer.dropEffect = 'move'
 			event.dataTransfer.effectAllowed = 'move'
-			event.dataTransfer.setData('itemId', item.id)
+			event.dataTransfer.setData('card', JSON.stringify(card))
 		},
-		onDrop (event, list) {
-			const itemId = event.dataTransfer.getData('itemId')
-			const item = this.listItems.find((item) => item.id === Number(itemId))
-			item.list = list
+		onDrop (event, index) {
+			let card = JSON.parse(event.dataTransfer.getData('card'))
+			this.lists[index]['cards'].push(card)
+
+			// draggable card should be removed from the origin list.
+			let cards = this.lists[this.draggableCardOriginListId]['cards']
+			let cardIndex = cards.map(c => c.id).indexOf(card.id)
+			cards = cards.splice(cardIndex, 1)
 		}
 	},
 }
