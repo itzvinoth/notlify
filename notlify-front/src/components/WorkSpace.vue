d6571cea72c6a87175ecbd672333ca8afbfaa981
mevinoth <vinoth.vinothrenga@gmail.com>
2022-05-24 10:19:04 +0530

    test: drag and drop within the list ui initiating

--- notlify-front/src/components/WorkSpace.vue
+++ notlify-front/src/components/WorkSpace.vue
@@ -25,10 +25,45 @@
 				</div>
 			</div>
 		</div>
+		<div id="list">
+			<div class="draggable" @mousedown="mouseDown">A</div>
+			<div class="draggable" @mousedown="mouseDown">B</div>
+			<div class="draggable" @mousedown="mouseDown">C</div>
+			<div class="draggable" @mousedown="mouseDown">D</div>
+			<div class="draggable" @mousedown="mouseDown">E</div>
+		</div>
 	</div>
 </template>
 
 <script>
+let draggingEle;
+let placeholder;
+let isDraggingStarted = false;
+// The current position of mouse relative to the dragging element
+let x = 0;
+let y = 0;
+
+// Swap two nodes
+const swap = function (nodeA, nodeB) {
+	const parentA = nodeA.parentNode;
+	const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+	// Move `nodeA` to before the `nodeB`
+	nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+	// Move `nodeB` to before the sibling of `nodeA`
+	parentA.insertBefore(nodeB, siblingA);
+};
+
+// Check if `nodeA` is above `nodeB`
+const isAbove = function (nodeA, nodeB) {
+	// Get the bounding rectangle of nodes
+	const rectA = nodeA.getBoundingClientRect();
+	const rectB = nodeB.getBoundingClientRect();
+
+	return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
+};
+
 export default {
 	name: 'WorkSpace',
 	data () {
@@ -83,6 +118,80 @@ export default {
 		}
 	},
 	methods: {
+		mouseDown (e) {
+			draggingEle = e.target;
+			// Calculate the mouse position
+			const rect = draggingEle.getBoundingClientRect();
+			x = e.pageX - rect.left;
+			y = e.pageY - rect.top;
+			document.addEventListener('mousemove', this.mouseMoveHandler);
+			document.addEventListener('mouseup', this.mouseUpHandler);
+		},
+		mouseMoveHandler (e) {
+			const draggingRect = draggingEle.getBoundingClientRect();
+			if (!isDraggingStarted) {
+				isDraggingStarted = true;
+
+				// Let the placeholder take the height of dragging element
+				// So the next element won't move up
+				placeholder = document.createElement('div');
+				placeholder.classList.add('placeholder');
+				draggingEle.parentNode.insertBefore(placeholder, draggingEle.nextSibling);
+				placeholder.style.height = `${draggingRect.height}px`;
+			}
+			// Set position for dragging element
+			draggingEle.style.position = 'absolute';
+			draggingEle.style.top = `${e.pageY - y}px`;
+			draggingEle.style.left = `${e.pageX - x}px`;
+			// The current order
+			// prevEle
+			// draggingEle
+			// placeholder
+			// nextEle
+			const prevEle = draggingEle.previousElementSibling;
+			const nextEle = placeholder.nextElementSibling;
+
+			// The dragging element is above the previous element
+			// User moves the dragging element to the top
+			if (prevEle && isAbove(draggingEle, prevEle)) {
+				// The current order    -> The new order
+				// prevEle              -> placeholder
+				// draggingEle          -> draggingEle
+				// placeholder          -> prevEle
+				swap(placeholder, draggingEle);
+				swap(placeholder, prevEle);
+				return;
+			}
+
+			// The dragging element is below the next element
+			// User moves the dragging element to the bottom
+			if (nextEle && isAbove(nextEle, draggingEle)) {
+				// The current order    -> The new order
+				// draggingEle          -> nextEle
+				// placeholder          -> placeholder
+				// nextEle              -> draggingEle
+				swap(nextEle, placeholder);
+				swap(nextEle, draggingEle);
+			}
+		},
+		mouseUpHandler () {
+			// Remove the placeholder
+			placeholder && placeholder.parentNode.removeChild(placeholder);
+
+			draggingEle.style.removeProperty('top');
+			draggingEle.style.removeProperty('left');
+			draggingEle.style.removeProperty('position');
+
+			x = null;
+			y = null;
+			draggingEle = null;
+			isDraggingStarted = false;
+
+			// Remove the handlers of `mousemove` and `mouseup`
+			document.removeEventListener('mousemove', this.mouseMoveHandler);
+			document.removeEventListener('mouseup', this.mouseUpHandler);
+		},
+
 		runQueue () {},
 		cardHandler (type, key) {
 			this.listKey = key
