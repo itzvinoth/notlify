fd91186a3d5e998e5c7d028db6e56f85e7d2ada3
mevinoth <vinoth.vinothrenga@gmail.com>
2022-06-09 07:53:56 +0530

    chore: card draganddrop

--- notlify-front/src/components/kanban/Card.vue
+++ notlify-front/src/components/kanban/Card.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="kb__column--cards">
-		<div class="kb__card" draggable="true" v-for="card in cards" :key="card.id" :data-id="card.id" @dragstart="onDragStart($event, card.id)">
+		<div class="kb__card" draggable="true" v-for="card in cards" :key="card.id" :data-id="card.id" @dragstart="onDragStart($event, card.id)" @dragenter.prevent @dragover.prevent>
 			<div class="kb__card--input" contenteditable>{{ card.title }}</div>
 		</div>
 	</div>
