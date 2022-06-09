109fefe9316a6d7b1ad1e21103958bc95dcd9ad6
mevinoth <vinoth.vinothrenga@gmail.com>
2022-06-09 10:46:49 +0530

    chore: css classname change

--- notlify-front/src/components/kanban/Card.vue
+++ notlify-front/src/components/kanban/Card.vue
@@ -2,13 +2,19 @@
 	<div class="kb__column--cards">
 		<div class="kb__card" draggable="true" v-for="card in cards" :key="card.id" :data-id="card.id" @dragstart="onDragStart($event, card.id)" @dragenter.prevent @dragover.prevent>
 			<div class="kb__card--input" contenteditable>{{ card.title }}</div>
+			<drop-zone />
 		</div>
 	</div>
 </template>
 
 <script>
+import DropZone from "@/components/kanban/DropZone.vue";
+
 export default {
 	name: 'card',
+	components: {
+		'drop-zone': DropZone
+	},
 	props: {
 		cards: {
 			type: Object
--- notlify-front/src/components/kanban/DropZone.vue
+++ notlify-front/src/components/kanban/DropZone.vue
@@ -1,5 +1,18 @@
 <template>
-	<div class="kb__dropzone">
-		
+	<div class="kb__dropzone" @drop="handleDrop" @dragenter.prevent @dragover.prevent>
+
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'DropZone',
+	methods: {
+		handleDrop (e) {
+			e.preventDefault()
+			let dropZone = document.getElementsByClassName('kb__dropzone')[0]
+			dropZone.classList.add('kb__dropzone--active')
+		}
+	},
+}
+</script>
