36547a96a1e50b50f5ca86cf90568e9025a8af42
Vinoth Kumar V <vinoth.kumar@zerodha.com>
2022-06-09 19:53:32 +0530

    chore: card dragLeaveHandler

--- notlify-front/src/components/kanban/DropZone.vue
+++ notlify-front/src/components/kanban/DropZone.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="kb__dropzone" @drop="checkDrop">
+	<div class="kb__dropzone" @dragover="dragOverHandler" @dragleave="dragLeaveHandler">
 
 	</div>
 </template>
@@ -13,11 +13,16 @@ export default {
 		}
 	},
 	methods: {
-		checkDrop (e) {
+		dragOverHandler (e) {
 			e.preventDefault()
 			let dropZone = document.getElementsByClassName('kb__card')
 			let elems = Array.from(dropZone).filter(el => Number(el.getAttribute('data-id')) === this.cardId)
 			elems.forEach(el => el.classList.add('kb__dropzone--active'))
+		},
+		dragLeaveHandler (e) {
+			e.preventDefault()
+			let dropZone = Array.from(document.getElementsByClassName('kb__card'))
+			dropZone.forEach(el => el.classList.remove('kb__dropzone--active'))
 		}
 	},
 }
