91e7e116a511f15e83c50d462be469860148082e
mevinoth <vinoth.vinothrenga@gmail.com>
2022-07-09 11:17:25 +0530

    link add in main page

--- notlify-front/src/views/HomeView.vue
+++ notlify-front/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import WorkSpace from "@/components/WorkSpace.vue";
 
 <template>
 	<main>
+		<div class="display: block;"><a href="/new" target="_blank">New</a></div>
 		<WorkSpace />
 	</main>
 </template>
