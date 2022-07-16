1eb52f73f717c90f951e6a72cd227e30ba0c691a
mevinoth <vinoth.vinothrenga@gmail.com>
2022-07-16 21:25:39 +0530

    on mount dropdown is in viewport check

--- notlify-front/src/components/kanban/CardDropdown.vue
+++ notlify-front/src/components/kanban/CardDropdown.vue
@@ -8,13 +8,18 @@
 </template>
 
 <script>
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth);
+}
+
 export default {
 	props: {
 		pos: {
 			type: String
 		}
 	},
-	mounted() {
+	mounted () {
 		let elem = document.getElementsByClassName('menu-dropdown')[0]
 		let rect = elem.getBoundingClientRect()
 		if (this.pos === 'top') {
@@ -25,6 +30,12 @@ export default {
 			this.setRight(elem, rect)
 		} else if (this.pos === 'left') {
 			this.setLeft(elem, rect)
+		}
+		// check if the "menu-dropdown" is in viewport
+		if (!isInViewport(elem)) {
+			let triggeringElem = elem.closest('.popover')
+			let rectTriggeringElem = triggeringElem.getBoundingClientRect()
+			elem.style.top = `${window.innerHeight - rectTriggeringElem.bottom - rect.height}px`
 		}
 	},
 	methods: {
