1b325621e28753f2b0e18f06d545ed7ec6214f49
mevinoth <vinoth.vinothrenga@gmail.com>
2022-07-25 16:34:26 +0530

    modal scss file add

---
+++ notlify-front/src/components/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+	<transition name="modal">
+		<div class="modal-mask">
+			<div class="modal-wrapper">
+				<div class="modal-container">
+				</div>
+			</div>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+    name: 'Modal'
+}
+</script>
