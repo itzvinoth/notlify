be7b192692ad9555578ebd30afbfe4153499370b
mevinoth <vinoth.vinothrenga@gmail.com>
2022-07-25 22:15:23 +0530

    adding vueuse core library and fix modal outside click close

--- notlify-front/src/components/Modal.vue
+++ notlify-front/src/components/Modal.vue
@@ -2,7 +2,7 @@
 	<Transition name="modal">
 		<div v-if="show" class="modal-mask">
 			<div class="modal-wrapper">
-				<div class="modal-container">
+				<div class="modal-container" ref="target">
 					<div class="modal-header">
 						<slot name="header">default header</slot>
 					</div>
@@ -27,10 +27,25 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+// https://vueuse.org/core/onclickoutside/#onclickoutside
+import { onClickOutside } from "@vueuse/core";
+
+const modal = ref(null)
+
 export default {
 	name: 'Modal',
 	props: {
 		show: Boolean
+	},
+	setup (props, contenxt) {
+		// https://vuejs.org/api/composition-api-setup.html#setup-context
+		const target = ref(null)
+		onClickOutside(target, (event) => {
+			contenxt.emit('close')
+		})
+
+		return { target }
 	}
 }
 </script>
