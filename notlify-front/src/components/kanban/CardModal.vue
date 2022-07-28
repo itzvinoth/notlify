88eb286a77bd87caece9f1a36fbeca590d8fd102
mevinoth <vinoth.vinothrenga@gmail.com>
2022-07-28 18:27:11 +0530

    action buttons added

--- notlify-front/src/components/kanban/CardModal.vue
+++ notlify-front/src/components/kanban/CardModal.vue
@@ -2,10 +2,25 @@
     <div>
         <Teleport to="body">
 			<!-- use the modal component, pass in the prop -->
-			<modal :show="show" @close="onCloseModal">
+			<modal :show="show" @close="onCloseModal" class="card-detail__modal">
 				<template #header>
 					<h3>{{ cardDetail.title }}</h3>
 				</template>
+                <template #body>
+                    <div class="row">
+                        <div class="side-panel three cols">
+                            <label>Add to card</label>
+                            <div class="card-action__container">
+                                <a class="button-action" href="#">Notes</a>
+                                <a class="button-action" href="#">Checklist</a>
+                                <a class="button-action" href="#">Reminder</a>
+                                <a class="button-action" href="#">Color</a>
+                                <a class="button-action" href="#">Attachment</a>
+                            </div>
+                        </div>
+                        <div class="main-panel nine cols">main panel</div>
+                    </div>
+                </template>
 			</modal>
 		</Teleport>
     </div>
