94dbec9f87daf0d96aa740f66db472ac892e1572
mevinoth <vinoth.vinothrenga@gmail.com>
2022-08-13 12:34:12 +0530

    init: Notes file

---
+++ notlify-front/src/components/kanban/Notes.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="notes">
+		<div class="notes-sidebar">
+			<button>Add a note</button>
+			<div class="notes-list">
+				<div class="notes-list__item">
+					<div class="notes-title">Title</div>
+					<div class="notes-body">Body on the contents</div>
+					<div class="notes-updated__time">Time</div>
+				</div>
+			</div>
+		</div>
+		<div class="notes-editor">
+			<div class="notes-title">Title</div>
+			<div class="notes-body">Body of the contents</div>
+		</div>
+	</div>
+</template>
