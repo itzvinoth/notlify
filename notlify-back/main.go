aa3f37c895a9249c75974c18ebb250c25c102174
mevinoth <vinoth.vinothrenga@gmail.com>
2023-02-11 18:44:23 +0530

    basic chi server setup

--- notlify-back/main.go
+++ notlify-back/main.go
@@ -3,54 +3,64 @@ package main
 import (
     // "fmt"
     "net/http"
-	
-	"github.com/gin-gonic/gin"
+    "log"
+    // "os"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
 )
 
 type card struct {
     ID    int   `json:"id"`
 }
 
-// board represents data about a record board.
-type board struct {
-    ID      int  `json:"id"`
-    Title   string  `json:"title"`
-    Cards   []card   `json:"cards"`
-}
+// // board represents data about a record board.
+// type board struct {
+//     ID      int  `json:"id"`
+//     Title   string  `json:"title"`
+//     Cards   []card   `json:"cards"`
+// }
 
-// boards slice to seed record board data.
-var boards = []board{
-    {ID: 356, Title: "User manual", Cards: []card{
-        card{
-            ID: 35656,
-        },
-    }},
-    {ID: 899, Title: "Things to try", Cards: []card{
-        card{
-            ID: 75050,
-        },
-    }},
-    {ID: 740, Title: "More to try", Cards: []card{
-        card{
-            ID: 44646,
-        },
-    }},
-}
+// // boards slice to seed record board data.
+// var boards = []board{
+//     {ID: 356, Title: "User manual", Cards: []card{
+//         card{
+//             ID: 35656,
+//         },
+//     }},
+//     {ID: 899, Title: "Things to try", Cards: []card{
+//         card{
+//             ID: 75050,
+//         },
+//     }},
+//     {ID: 740, Title: "More to try", Cards: []card{
+//         card{
+//             ID: 44646,
+//         },
+//     }},
+// }
 
 // func hello(w http.ResponseWriter, req *http.Request) {
 //     fmt.Fprintf(w, "hello\n")
 // }
 
 // getBoards responds with the list of all boards as JSON.
-func getBoards(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, boards)
-}
+// func getBoards(c *gin.Context) {
+//     c.IndentedJSON(http.StatusOK, boards)
+// }
 
 func main() {
-    router := gin.Default()
-    router.GET("/api/boards", getBoards)
+    port := "3001"
+    // router := gin.Default()
+    // router.GET("/api/boards", getBoards)
 
-    router.Run("localhost:8080")
-    // http.HandleFunc("/hello", hello)
-    // http.ListenAndServe(":8080", nil)
+    // router.Run("localhost:8080")
+    log.Printf("Starting up on http://localhost:%s", port)
+
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    http.ListenAndServe(":"+port, r)
 }
